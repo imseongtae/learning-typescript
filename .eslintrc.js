7046c06d6667cb5e438fe13f598c654aca6461d9
imseongtae <dev.haemil@gmail.com>
2023-09-21 19:19:05 +0900

    [chore] Init project

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'prettier'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended', // Prettier 규칙 및 ESLint와의 통합을 위한 설정을 추가합니다.
+    'prettier'
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    // example) '@typescript-eslint/explicit-function-return-type': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 100,
+        singleQuote: true,
+        semi: true,
+        useTabs: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+      },
+    ],
+  }
+};
